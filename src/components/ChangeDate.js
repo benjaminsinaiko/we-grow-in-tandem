c85ec4e385f9a1576c9e39f7bcc8fad5ccababae
benjaminsinaiko <ben.jamin@outlook.com>
2019-12-14 02:44:32 -0600

    WIP - Date change updates local state, needs global

--- src/components/ChangeDate.js
+++ src/components/ChangeDate.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -8,7 +8,7 @@ import SubtractIcon from '@material-ui/icons/Remove';
 import Typography from '@material-ui/core/Typography';
 
 import COLORS from '../utils/colors';
-import { displayRangeFormat } from '../utils/dateHelpers';
+import { displayRangeFormat, isInvalidDate, changeByOneDay } from '../utils/dateHelpers';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,33 +28,15 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function getDay(date) {
-  return moment(date).day();
-}
-
-function changeByOneDay(date, direction) {
-  if (direction === 'increment') {
-    return moment(date).add(1, 'd');
-  } else if (direction === 'decrement') {
-    return moment(date).subtract(1, 'd');
-  } else {
-    return date;
-  }
-}
-
-function isInvalidDate(date, moveDirection) {
-  if (getDay(date) === 1 && moveDirection === 'decrement') {
-    return true;
-  }
-  if (getDay(date) === 5 && moveDirection === 'increment') {
-    return true;
-  }
-  return false;
-}
-
 export default function ChangeDate({ selectedPlant }) {
   const classes = useStyles();
   const [newDate, setNewDate] = useState(selectedPlant.start);
+  const [sameDate, setSameDate] = useState(true);
+
+  useEffect(() => {
+    const isSame = moment(selectedPlant.start).isSame(moment(newDate));
+    setSameDate(isSame);
+  }, [selectedPlant.start, newDate]);
 
   const handleChangeDate = useCallback(
     direction => {
@@ -63,9 +45,21 @@ export default function ChangeDate({ selectedPlant }) {
     [newDate]
   );
 
+  function handleSubmit() {
+    console.log(selectedPlant.id);
+    console.log(newDate);
+  }
+
   return (
     <div className={classes.root}>
-      <Typography variant='h6'>Adjust one day</Typography>
+      <Button
+        disabled={sameDate}
+        color={sameDate ? 'inherit' : 'primary'}
+        variant={sameDate ? 'text' : 'outlined'}
+        onClick={handleSubmit}
+      >
+        Change Day
+      </Button>
       <div className={classes.dateChangeControl}>
         <IconButton
           disabled={isInvalidDate(newDate, 'decrement')}
--- src/utils/dateHelpers.js
+++ src/utils/dateHelpers.js
@@ -11,3 +11,27 @@ export function displayRangeFormat(date) {
 export function displayInfoDate(date) {
   return moment(date).format('ddd, MMM Do YYYY');
 }
+
+export function getDay(date) {
+  return moment(date).day();
+}
+
+export function isInvalidDate(date, moveDirection) {
+  if (getDay(date) === 1 && moveDirection === 'decrement') {
+    return true;
+  }
+  if (getDay(date) === 5 && moveDirection === 'increment') {
+    return true;
+  }
+  return false;
+}
+
+export function changeByOneDay(date, direction) {
+  if (direction === 'increment') {
+    return moment(date).add(1, 'd');
+  } else if (direction === 'decrement') {
+    return moment(date).subtract(1, 'd');
+  } else {
+    return date;
+  }
+}
